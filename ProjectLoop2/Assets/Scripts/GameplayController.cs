66f5a56675d9fa362e53e48dc60d4d6f83bca9b6
AntoineDutil <antoineberthiaumedutil@gmail.com>
2016-10-20 23:59:14 -0400

    First push.

---
+++ ProjectLoop2/Assets/Scripts/GameplayController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayController : MonoBehaviour {
+
+    int screenWith;
+
+    void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        screenWith = Screen.width;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.touchCount > 0)
+        {
+            Touch myTouch = Input.GetTouch(0);
+
+            if (myTouch.position.x > screenWith / 2)
+            {
+                Debug.Log("Right");
+            }
+            else
+            {
+                Debug.Log("Left");
+            }
+        }
+        
+	}
+}
